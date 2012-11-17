40ffb24cc4688f7e43ea8aa226a4e8a7aa11d58c
Bobby Ong <bobbyongce@gmail.com>
2012-11-17 18:41:22 +0800

    registered Network in admin panel - missed out in prev commit

--- shows/admin.py
+++ shows/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Show, Episode, Actor, Director, Genre
+from .models import Show, Episode, Actor, Director, Genre, Network
 
 admin.site.register(Actor)
 admin.site.register(Director)
 admin.site.register(Genre)
+admin.site.register(Network)
 admin.site.register(Show)
 admin.site.register(Episode)
