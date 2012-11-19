ce8bfeb421513eb5d8d1dcf9fabd6edf3c7e41ad
Bobby Ong <bobbyongce@gmail.com>
2012-11-19 21:57:41 +0800

    added front-end pages

--- bollywood/urls.py
+++ bollywood/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', TemplateView.as_view(template_name='home.html')),
-    url(r'^$', 'shows.views.show_list'),
+    url(r'^$', 'shows.views.list'),
+    url(r'^show/(?P<slug>[-\w]+)/$', 'shows.views.detail'),
 )
--- shows/views.py
+++ shows/views.py
@@ -2,13 +2,20 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from .models import Show
 
+from django.http import Http404
 
-def show_list(request):
+def list(request):
 	shows = Show.objects.all()
 	
 	return render_to_response('showlist.html',
             				{'shows': shows},
                             context_instance=RequestContext(request))
 
-def show_page(request):
-	pass
+def detail(request, slug):
+    try:
+        show = Show.objects.get(show_slug_name=slug)
+    except Show.DoesNotExist:
+        raise Http404
+    return render_to_response('showdetail.html',
+            				{'show': show},
+                            context_instance=RequestContext(request))
