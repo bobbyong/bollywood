f6990bd616337b8fee4fe53924baff648d295b82
Bobby Ong <bobbyongce@gmail.com>
2012-11-19 22:24:10 +0800

    added episode pages

--- bollywood/urls.py
+++ bollywood/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', TemplateView.as_view(template_name='home.html')),
-    url(r'^$', 'shows.views.list'),
-    url(r'^show/(?P<slug>[-\w]+)/$', 'shows.views.detail'),
+    url(r'^$', 'shows.views.show_list'),
+    url(r'^show/(?P<show_slug>[-\w]+)/$', 'shows.views.show_detail'),
+    url(r'^show/(?P<show_slug>[-\w]+)/(?P<episode_slug>[-\w]+)$', 'shows.views.episode_detail'),
 )
--- shows/views.py
+++ shows/views.py
@@ -1,21 +1,32 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from .models import Show
+from .models import Show, Episode
 
 from django.http import Http404
 
-def list(request):
+def show_list(request):
 	shows = Show.objects.all()
 	
 	return render_to_response('showlist.html',
             				{'shows': shows},
                             context_instance=RequestContext(request))
 
-def detail(request, slug):
+def show_detail(request, show_slug):
     try:
-        show = Show.objects.get(show_slug_name=slug)
+        show = Show.objects.get(show_slug_name=show_slug)
     except Show.DoesNotExist:
         raise Http404
     return render_to_response('showdetail.html',
             				{'show': show},
+                            context_instance=RequestContext(request))
+
+
+def episode_detail(request, show_slug, episode_slug):
+    try:
+        show = Show.objects.get(show_slug_name=show_slug)
+        episode = Episode.objects.get(episode_slug_name = episode_slug)
+    except Show.DoesNotExist:
+        raise Http404
+    return render_to_response('episodedetail.html',
+            				{'episode': episode},
                             context_instance=RequestContext(request))
