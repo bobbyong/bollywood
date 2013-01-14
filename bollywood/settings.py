05198884f3e424c7bfcc1acdabc9192109eb9fd0
Abhishek Gupta <abhishekgupta.it@gmail.com>
2013-01-13 18:48:50 -0800

    new feature in branch

--- bollywood/settings.py
+++ bollywood/settings.py
@@ -240,3 +240,17 @@ elif os.environ.get('BOBBY_MACHINE'):
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
     }
+elif os.environ.get('ABHISHEK_MACHINE'):
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'dekkho',                      # Or path to database file if using sqlite3.
+            'USER': 'postgres',                      # Not used with sqlite3.
+            'PASSWORD': 'postgres',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    }
